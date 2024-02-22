0d968b5ed17406b5e141e02dc6217d5a32b896a8
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-02-22 19:27:11 +0100

    commit

--- src/main/kotlin/coroutines/couritine.kt
+++ src/main/kotlin/coroutines/couritine.kt
@@ -1,16 +1,16 @@
+package coroutines
+
 import kotlinx.coroutines.*
 
 suspend fun fn() {
     delay(200)
 }
 
-fun main()=runBlocking {
-    GlobalScope.launch(Dispatchers.Default) {
+fun main() :Unit =runBlocking {
+    launch{
         for (i in 1..100) {
             println(i)
             delay(20)
         }
     }
-}
-
 }
